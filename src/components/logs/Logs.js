3983a70d496ec75e1aa6a3811b4e6dd473f309ba
SilvesterSpath <51899620+SilvesterSpath@users.noreply.github.com>
2021-06-29 08:23:20 +0200

    logReducer

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import LogItem from './LogItem';
 import Preloader from '../layout/Preloader';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getLogs } from '../../actions/logActions';
 
-const Logs = () => {
-  const [logs, setLogs] = useState([]);
-  const [loading, setLoading] = useState(false);
-
+const Logs = ({ log: { logs, loading }, getLogs }) => {
   useEffect(() => {
     getLogs();
     // seling-desable-next-line
   }, []);
 
-  const getLogs = async () => {
+  /*   const getLogs = async () => {
     setLoading(true);
     const res = await fetch('/logs');
 
@@ -19,9 +19,9 @@ const Logs = () => {
 
     setLogs(data);
     setLoading(false);
-  };
+  }; */
 
-  if (loading) {
+  if (loading || logs === null) {
     return <Preloader />;
   }
 
@@ -39,4 +39,12 @@ const Logs = () => {
   );
 };
 
-export default Logs;
+Logs.protoTypes = {
+  log: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  log: state.log,
+});
+
+export default connect(mapStateToProps, { getLogs })(Logs);
