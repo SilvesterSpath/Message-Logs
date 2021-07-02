41bd15ce30015c3b7e98fa8bf2835b00dec8a34c
SilvesterSpath <51899620+SilvesterSpath@users.noreply.github.com>
2021-07-02 05:53:56 +0200

    delete function

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,4 +1,10 @@
-import { ADD_LOG, GET_LOGS, LOGS_ERROR, SET_LOADING } from '../actions/types';
+import {
+  ADD_LOG,
+  DELETE_LOG,
+  GET_LOGS,
+  LOGS_ERROR,
+  SET_LOADING,
+} from '../actions/types';
 
 const initialState = {
   logs: null,
@@ -20,6 +26,13 @@ const logReducer = (state = initialState, action) => {
       return {
         ...state,
         logs: [...state.logs, action.payload],
+        loading: false,
+      };
+
+    case DELETE_LOG:
+      return {
+        ...state,
+        logs: state.logs.filter((i) => i.id !== action.payload),
         loading: false,
       };
 
