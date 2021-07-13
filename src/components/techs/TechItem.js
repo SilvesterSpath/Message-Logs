c4e319c283aaca9e9debedef5decab8957e7e6f7
SilvesterSpath <51899620+SilvesterSpath@users.noreply.github.com>
2021-07-13 06:36:34 +0200

    firstName

--- src/components/techs/TechItem.js
+++ src/components/techs/TechItem.js
@@ -14,7 +14,7 @@ const TechItem = ({ tech, deleteTech }) => {
   return (
     <li className='collection-item'>
       <div>
-        {tech.firsName} {tech.lastName}
+        {tech.firstName} {tech.lastName}
         <a href='#!' className='secondary-content'>
           <i className='material-icons grey-text' onClick={onDelete}>
             delete
